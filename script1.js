65531eaf62027fcebf7ecfb24426b5f058767410
chzxtw <chzxtw@gmail.com>
2023-07-18 21:48:12 -0300

    Primeiro Script

---
+++ script1.js
@@ -0,0 +1,80 @@
+ //Crie 3 Variaveis contendo numeros e crie 3 variaveis tendo a 1° + 2° * 3° -
+// let num1 = 16
+//     let num2 = 6
+//     let num3 = 25
+// let soma = num1+num2+num3
+//     console.log("A primeira soma é "+soma)
+// let soma2 = num1*num2*num3
+//         console.log("A segunda soma é "+soma2)
+// let soma3 = num1-num2-num3
+//             console.log("A terceira soma é "+soma3)
+
+     //Calcule a Media de 3 notas se ele tirou mais que a media tem que aparecer aprovado se for o contrario reprovado
+// const mat = 7;
+// const por = 8;
+// const ing = 5;
+//     let adição = mat+por+ing;
+//     let dvis = adição/3;
+//         console.log(dvis)
+//     if (dvis>5){
+//         console.log("Aprovado")
+//     }
+//     if (dvis<5){
+//         console.log("Reprovado")
+//     }
+//     if (dvis==5){
+//         console.log("Aprovado Mais na Media 🥵")
+//     }
+
+//Saber Sua Idade De Acordo Com o Ano De Nascimento
+//  let nasc = 2007;
+//   let ano = new Date().getFullYear();
+//    let year = ano-nasc;
+// console.log("Sua idade é " + year)
+
+//  Convertor de Graus para Fahrenheit
+
+// let graus = 38 ;
+//  let multplicação = 1.8;
+//   let adição = 32;
+//    let fahrenheit = graus * multplicação + adição;
+// console.log("Este valor em fahrenheit é " + fahrenheit );
+
+ //Convertor de Fahrenheit para Graus
+
+// let fahrenheit = 100.4;
+//  let subtração = 32.0;
+//   let divisão = 1.8;
+//    let graus = fahrenheit - subtração;
+//     let resultado = graus / divisão
+// console.log("Este valor em graus é " + resultado );
+
+//Faça um programa que retorna se uma determinada pessoa pode dirigir, baseado na sua idade.
+
+// let nasc = 1     ;
+//  let ano = 2023;
+//   let idade = ano - nasc;
+
+
+// if (idade > 18){
+//      console.log("Voçê pode dirigir!")
+// }
+// else if (idade < 18){
+//      console.log("Voçê não pode dirigir!")
+// }
+// else if (idade == 18){
+//      console.log("Ja ta na hora de tirar carta hein!")
+// }
+// if (idade > 99){
+//      console.log("Voce ja morreu!!")
+// }
+ 
+ //Escreva um programa que verifique se um número é par ou ímpar.
+let numero = 65745764;
+
+  if (numero % 2 === 0) {
+    console.log(numero + " é um número PAR.");
+  } 
+  else {
+    console.log(numero + " é um número ÍMPAR.");
+  }
